3d539e93be0a7279522ccb1294217e8d520ed0be
Duy An <duyan75vt@gmail.com>
2017-11-26 00:59:15 +0700

    new app build

--- front-end/packages/hr/src/js/config/config-production.js
+++ front-end/packages/hr/src/js/config/config-production.js
@@ -1,4 +1,4 @@
 export const configs = {
-  API_URL: `${window.location.protocol}//peopletrek.org/api`,
+  API_URL: `${window.location.protocol}//meditrak.online/api`,
   FILE_HOST: 'https://s3-us-west-1.amazonaws.com/honeycomb-prod',
 };
